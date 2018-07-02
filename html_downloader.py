ef49d244db59f21d6a1dcca828fba92ef38af480
fm <33831200+fmingzhang@users.noreply.github.com>
2018-07-02 20:41:45 +0800

    Update html_downloader.py

--- html_downloader.py
+++ html_downloader.py
@@ -4,8 +4,7 @@ class HtmlDownloader(object):
     def download(self, url):
         if url is None:
             return None
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36'}
+        headers = {}
         rp=requests.get(url,headers=headers)
         if rp.status_code!=200:
             return None
